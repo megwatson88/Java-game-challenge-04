a9307ef24414134f1d14a172d8033acba00cd595
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-09 13:03:37 -0700

    addtions

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,35 @@
-document.addEventListener(DOMContentloaded )
+function generateQuiz(question, quizContainer, resultsContainer, submitButton) {
+    function showQuestions(questions, questionsContainer) {
+
+    }
+    function showResults(questoins, quizContainer, resultsContainer) {
+
+    }
+    showQuestions(questions, quizContainer);
+
+    submitButton.onclick = fucntion(); {
+        showResults(questions, quizContainer, resultsContianer);
+    }
+}
+var myQuestions = [
+    {
+        question: "What is a function",
+        answers: {
+            a: 'A procedure to perform tasks or calculate a value.',
+            b: 'y=mx+b',
+            c: 'Direct Object Model',
+            d: 'Application Programing Interface'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is pseudocode? "
+            answers: {
+            a: 'A plain language description of the algorithm ',
+            b: 'A partial language of pseudonyms ',
+            c: 'A partial list of truths ',
+            d: 'A code that is partial'
+        },
+        correctAnswer = a
+    }
+]
